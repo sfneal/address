fcd053b8c7b4f7326ae5b283e3ce5269149e2f31
Stephen Neal <sfneal@users.noreply.github.com>
2020-10-27 16:06:42 +0000

    Apply fixes from StyleCI

--- src/Models/Address.php
+++ src/Models/Address.php
@@ -108,7 +108,7 @@ class Address extends AbstractModel
     public function setCityAttribute($value)
     {
         if (isset($value) && inString($value, ',')) {
-            list($city, $state) = explode(',', $value);
+            [$city, $state] = explode(',', $value);
             $this->attributes['city'] = ucfirst(trim($city));
             $this->setStateAttribute($state);
         } else {
